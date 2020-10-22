e060904f320d28bb42575ffcf7d776e0ed250268
Ganesh Katrapati <ganesh@swecha.net>
2020-10-22 16:40:46 +0530

    Try Catch Block for killing processes

--- ws-server.py
+++ ws-server.py
@@ -17,8 +17,14 @@ import psutil
 def kill(proc_pid):
     process = psutil.Process(proc_pid)
     for proc in process.children(recursive=True):
-        proc.kill()
-    process.kill()
+        try:
+            proc.kill()
+        except:
+            pass
+    try:
+        process.kill()
+    except:
+        pass
 
 class Status:
     STATUS = "idle"
